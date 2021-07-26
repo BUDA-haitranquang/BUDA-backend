4097080eb14f102ac5be99194f6b32c6fa83f56d
Tiennd4857 <tien.nd194857@sis.hust.edu.vn>
2021-07-26 21:40:21 +0700

    Convert to React Hooks

--- Project/Buda/src/main/frontend/src/pages/Signup.jsx
+++ Project/Buda/src/main/frontend/src/pages/Signup.jsx
@@ -1,80 +1,52 @@
-import react, { Component, Fragment } from 'react';
-import "./Signup.css"
-import ava from "../img/ava.png"
+import react, { useState, Fragment } from 'react';
+import "./Signup.css";
+import ava from "../img/ava.png";
 import { Redirect } from 'react-router-dom';
-export default class Signup extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            firstname: '',
-            lastname: '',
-            email: '',
-            phone: '',
-            password: '',
-            checkpassword: ''
-        }
-    }
-    setFirstName = e =>{
-        this.setState({firstname: e.target.value})
-    }
-    setLastName = e =>{
-        this.setState({lastname: e.target.value})
-    }
-    setEmail = e =>{
-        this.setState({email: e.target.value})
-    }
-    setPhone = e =>{
-        this.setState({phone: e.target.value})
-    }
-    setPassword = e =>{
-        const pass = e.target.value;
-        if(pass.length < 6){
-            console.log("Password is too short");
-        }
-        else this.setState({password: pass})
-    }
-    setCheckpassword = e =>{
-        this.setState({checkpassword: e.target.value})
-    }
-    checkPasswordDuplicate = ()=>{
-        console.log(this.state.password);
-        console.log(this.state.checkpassword);
-        if(this.state.checkpassword !== this.state.password)
+export default function Signup() {
+    const [firstname, setFirstname] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [phone, setPhone] = useState('')
+    const [checkpassword, setCheckpassword] = useState('')
+    
+    const checkPasswordDuplicate = ()=>{
+        if(checkpassword !== password) 
         return(
             <p style={{color: 'red', textAlign: 'left', marginLeft: '5%'}}>* Passwords are not identical</p>
         )
-        
+        else{}
     }
-    submitRegister = () =>{
-        console.log(this.state);
-        
+    //TODO: add condition for check Password
+    const submitRegister = (e) =>{
+        const item ={firstname, lastname, email, phone, password, checkpassword}
+        console.log(item);
     }
-    render(){
-        return(
-            <Fragment>
-            <div className="signup-container">
-                <div className="ava">
-                    <img src={ava}/>
-                </div>
-                <form>
-                    <ul>
-                    <li><input type="text" placeholder="First Name" value={this.state.firstname} onChange={this.setFirstName} required/></li>
-                    <li><input type="text" placeholder="Last Name" value={this.state.lastname} onChange={this.setLastName}/></li>
-                    <li><input type="text" placeholder="Email" value={this.state.email} onChange={this.setEmail}/></li>
-                    <li><input type="text" placeholder="Phone" value={this.state.phone} onChange={this.setPhone}/></li>
-                    <li><input type="password" placeholder="Password" onChange={this.setPassword}/></li>
-                    <li>
-                        <input type="password" placeholder="Reenter Password" onChange={this.setCheckpassword}/>
-                        {this.checkPasswordDuplicate()}
-                    </li>
-                    
-                    </ul>
-                    <div className="clearfix"></div>
-                    <input type="submit" onClick={this.submitRegister} value="Signup"/>
-                </form>
+
+    return(
+        <Fragment>
+        <div className="signup-container">
+            <div className="ava">
+                <img src={ava}/>
             </div>
-        </Fragment>
-        )
-      
-    }
+            <form onSubmit={e => e.preventDefault()} >
+                <ul>
+                <li><input type="text" placeholder="First Name" value={firstname} onChange={(e)=>setFirstname(e.target.value)} required/></li>
+                <li><input type="text" placeholder="Last Name" value={lastname} onChange={(e)=>setLastname(e.target.value)} required/></li>
+                <li><input type="text" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} required/></li>
+                <li><input type="text" placeholder="Phone" value={phone} onChange={(e)=>setPhone(e.target.phone)} required/></li>
+                <li><input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} required/></li>
+                <li>
+                    <input type="password" placeholder="Reenter Password" value={checkpassword} onChange={(e)=>setCheckpassword(e.target.value)} required/>
+                    {checkPasswordDuplicate()}
+                </li>
+                
+                </ul>
+                <div className="clearfix"></div>
+                <input type="submit" onClick={()=>submitRegister} value="Signup"/>
+            </form>
+        </div>
+    </Fragment>
+    )
+
   }
