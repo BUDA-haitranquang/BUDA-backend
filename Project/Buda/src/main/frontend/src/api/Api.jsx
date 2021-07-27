811ac17b4c614aaefd466f2461fe9b20c72b6996
Tiennd4857 <tien.nd194857@sis.hust.edu.vn>
2021-07-27 16:28:03 +0700

    Convert Login page to React Hooks

--- Project/Buda/src/main/frontend/src/api/Api.jsx
+++ Project/Buda/src/main/frontend/src/api/Api.jsx
@@ -35,6 +35,24 @@ function postApi(data){
 
     return <p>API posted</p>
 }
+function patchApi(data){
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let requestOptions = {
+        method: 'PATCH',
+        headers: myHeaders,
+        body: JSON.stringify(data),
+        redirect: 'follow'
+    };
+
+    fetch(urlApi, requestOptions)
+    .then(response => response.text())
+    .then(result => {console.log('Success: ', result)})
+    .catch(error => console.log('error: ', error));
+
+    return <p>API posted</p>
+}
 
 function deleteApi(id){
     let myHeaders = new Headers();
@@ -52,8 +70,10 @@ function deleteApi(id){
 
     return <p>API posted</p>
 }
-// export{
-//     getApi,
-//     postApi
-// }
-export default postApi;
+export{
+    getApi,
+    postApi,
+    patchApi,
+    deleteApi
+}
+export default getApi;
--- Project/Buda/src/main/frontend/src/pages/Login.jsx
+++ Project/Buda/src/main/frontend/src/pages/Login.jsx
@@ -2,9 +2,26 @@ import react, { Component, Fragment, useState } from 'react';
 import "./Login.css"
 import ava from "../img/ava.png"
 export default function Login () {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isRemember, setIsRemember] = useState(false);
+    const [inputs, setInputs] = useState({
+        username: '',
+        password: ''
+    })
+    const [submitted, setSubmitted] = useState(false)
+    const{username, password} = inputs;
+    //const [isRemember, setIsRemember] = useState(false);
+    
+    function handleChange(e) {
+        const{name, value} = e.target;
+        setInputs(inputs=>({...inputs, [name]: value}))
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setSubmitted(true)
+        if(username && password){
+            
+        }
+    }
     return(
         <div className="login-container">
             <div className="ava">
@@ -12,13 +29,14 @@ export default function Login () {
             </div>
             <form>
                 <ul>
-                <li><input type="text" placeholder="Email/Phone" required/></li>
+                <li><input type="text" placeholder="Email/Phone" value={inputs.username} onChange={handleChange} required/></li>
                 <li><input type="password" placeholder="Password" required/></li>
                 </ul>
-                <label><input type="checkbox" value="isRemember" />Remember me</label>
+                {/* TODO: Add remember me */}
+                {/* <label><input type="checkbox" value="isRemember" />Remember me</label> */}
                 <span>Forgot Password?</span>
                 <div className="clearfix"></div>
-                <input type="submit" onClick="isRememberMe" value="Login"/>
+                <input type="submit" onClick={()=>handleSubmit()} value="Login"/>
             </form>
         </div>
     )
