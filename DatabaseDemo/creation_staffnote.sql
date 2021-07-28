fa722d8211dc190ad8b440873f54e1fa581fab34
linhngocnguyen <linh.nn194790@sis.hust.edu.vn>
2021-07-28 21:32:35 +0700

    Add AutoIncrement

--- DatabaseDemo/creation_staffnote.sql
+++ DatabaseDemo/creation_staffnote.sql
@@ -8,8 +8,6 @@
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
@@ -19,20 +17,19 @@
 -- Table structure for table `staffnote`
 --
 
-DROP TABLE IF EXISTS `staffnote`;
+DROP TABLE IF EXISTS staffnote;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `staffnote` (
-  `StaffID` int DEFAULT NULL,
-  `UserID` int DEFAULT NULL,
-  `NoteDate` date DEFAULT NULL,
-  `Message` varchar(150) DEFAULT NULL,
-  `Is_seen` tinyint DEFAULT NULL,
-  KEY `EmployeeID_fk_idx` (`StaffID`),
-  KEY `UserID_fk_idx` (`UserID`),
-  KEY `UserID_fk1_idx` (`UserID`),
-  CONSTRAINT `StaffID_fk` FOREIGN KEY (`StaffID`) REFERENCES `staff` (`StaffID`) ON DELETE SET NULL,
-  CONSTRAINT `UserID_fk1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE SET NULL
+CREATE TABLE staffnote (
+  StaffID int DEFAULT NULL,
+  UserID int DEFAULT NULL,
+  NoteDate date DEFAULT NULL,
+  Message varchar(150) DEFAULT NULL,
+  Is_seen tinyint DEFAULT NULL,
+  KEY UserID_fk1_idx (UserID),
+  KEY StaffID_fk_idx (StaffID),
+  CONSTRAINT StaffID_fk FOREIGN KEY (StaffID) REFERENCES staff (StaffID) ON DELETE SET NULL,
+  CONSTRAINT UserID_fk15 FOREIGN KEY (UserID) REFERENCES `user` (UserID) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,12 +37,7 @@ CREATE TABLE `staffnote` (
 -- Dumping data for table `staffnote`
 --
 
-LOCK TABLES `staffnote` WRITE;
-/*!40000 ALTER TABLE `staffnote` DISABLE KEYS */;
-INSERT INTO `staffnote` VALUES (1,1,'2021-06-17','No comment',1),(3,1,'2021-06-25','Demo',1),(8,1,'2021-06-29','Testing',1),(6,1,'2021-07-04','Beta',1);
-/*!40000 ALTER TABLE `staffnote` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO staffnote VALUES (1,1,'2021-06-17','No comment',1),(3,1,'2021-06-25','Demo',1),(8,1,'2021-06-29','Testing',1),(6,1,'2021-07-04','Beta',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -55,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-27 10:49:30
+-- Dump completed
