960c3c445d9b44f81a168749e6704b04e4c5ea74
tranhaidang <tranhaidang01dtm@gmail.com>
2021-07-29 10:50:59 +0700

    Add BadRequestExceptionHandler

--- Project/Buda/src/main/java/com/higroup/Buda/exception/APIBadRequestException.java
+++ Project/Buda/src/main/java/com/higroup/Buda/exception/APIBadRequestException.java
@@ -1,5 +1,12 @@
 package com.higroup.Buda.exception;
 
-public class APIBadRequestException {
-    
+public class APIBadRequestException extends RuntimeException{
+
+    public APIBadRequestException(String message) {
+        super(message);
+    }
+
+    public APIBadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
