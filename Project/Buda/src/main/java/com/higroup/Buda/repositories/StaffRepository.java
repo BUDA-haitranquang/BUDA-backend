cdc96a60e8949be59e522a8c98ccbe2ab794019a
haitranquang-official <haitranquang.official@gmail.com>
2021-08-05 19:58:15 +0700

    Edit logic flow for Services' methods of several classes
    - Including SalaryLog, SellOrderItem, SellOrder, StaffNote, StaffService

--- Project/Buda/src/main/java/com/higroup/Buda/repositories/StaffRepository.java
+++ Project/Buda/src/main/java/com/higroup/Buda/repositories/StaffRepository.java
@@ -1,5 +1,6 @@
 package com.higroup.Buda.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.higroup.Buda.entities.Staff;
@@ -9,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StaffRepository extends JpaRepository<Staff, Long>{
     Optional<Staff> findStaffByLoginID(String loginID);
     Optional<Staff> findStaffByStaffID(Long staffID);
+    List<Staff> findAllByUserID(Long userID);
 }
--- Project/Buda/src/main/java/com/higroup/Buda/restcontroller/StaffNoteController.java
+++ Project/Buda/src/main/java/com/higroup/Buda/restcontroller/StaffNoteController.java
@@ -1,10 +1,18 @@
 package com.higroup.Buda.restcontroller;
 
+import java.util.List;
+
+import com.higroup.Buda.entities.StaffNote;
 import com.higroup.Buda.repositories.StaffNoteRepository;
 import com.higroup.Buda.services.StaffNoteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +26,19 @@ public class StaffNoteController {
     {
         this.staffNoteService = staffNoteService;
     }
-
+    @PostMapping(path = "userID/{userID}/register")
+    public ResponseEntity<?> registerNewStaffNote(@PathVariable Long userID, @RequestBody StaffNote staffNote)
+    {
+        return this.staffNoteService.registerNewStaffNote(userID, staffNote);
+    }
+    @GetMapping(path = "userID/{userID}/all")
+    public List<StaffNote> findAllByUserID(@PathVariable Long userID)
+    {
+        return this.staffNoteService.findAllByUserID(userID);
+    }
+    @GetMapping(path = "staffID/{staffID}/all")
+    public List<StaffNote> findAllByStaffID(@PathVariable Long staffID)
+    {
+        return this.staffNoteService.findAllByStaffID(staffID);
+    }
 }
