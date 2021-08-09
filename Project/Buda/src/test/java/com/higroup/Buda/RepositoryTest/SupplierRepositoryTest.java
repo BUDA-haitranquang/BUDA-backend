ca079a57cc578cef7e0d428238548dae287a8df5
nguyenhoangvudtm23 <68849645+nguyenhoangvudtm23@users.noreply.github.com>
2021-08-09 23:22:33 +0700

    supplier basic test update

--- Project/Buda/src/test/java/com/higroup/Buda/RepositoryTest/SupplierRepositoryTest.java
+++ Project/Buda/src/test/java/com/higroup/Buda/RepositoryTest/SupplierRepositoryTest.java
@@ -1,5 +1,13 @@
 package com.higroup.Buda.RepositoryTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import com.higroup.Buda.entities.Supplier;
+import com.higroup.Buda.entities.User;
 import com.higroup.Buda.repositories.SupplierRepository;
 import com.higroup.Buda.repositories.UserRepository;
 
@@ -28,13 +36,63 @@ public class SupplierRepositoryTest {
                     .withPassword("pass");
                     
     @Autowired
-    private SupplierRepository supplierRepository;
+    private SupplierRepository supplierReposTest;
     
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userReposTest;
 
+
+    private Supplier supplier;
+    private User newUser;
     @BeforeEach
+    public void setUp(){
+
+        newUser = new User();
+        newUser.setEmail("haitq@gmail.com");
+        newUser.setFirstName("Hai");
+        newUser.setLastName("Tran");
+        newUser.setPassword("BBBBBasdsadBB");
+        newUser.setPhoneNumber("21312313");
+        newUser.setUserName("haihoho");
+        userReposTest.save(newUser);
+
+        supplier = new Supplier();
+        supplier.setName("nguyen Hoang Vu");
+        supplier.setEmail("nguyenhoangvudtm23@gmail.com");
+        supplier.setPhoneNumber("0367185122");
+        supplier.setUserID(newUser.getUserID());
+        supplierReposTest.save(supplier);
+
+    }
+    
+    @AfterEach
     public void tearDown(){
-        
+        userReposTest.deleteAll();
+        supplierReposTest.deleteAll();
+    }
+
+    @Test 
+    public void canFindSupplierbySupplierID(){
+        Long supplierID = supplier.getSupplierID();
+
+        Optional<Supplier> res = supplierReposTest.findSupplierBySupplierID(supplierID);
+        assertEquals(res.get().getSupplierID(), supplier.getSupplierID());
+    }
+
+    @Test 
+    public void canFindAllbyUserID(){
+        Long userID = newUser.getUserID();
+        List<Supplier> list = supplierReposTest.findAllByUserID(userID);
+
+        assertEquals(list, Arrays.asList(supplier));
+    }
+
+    @Test
+    public void canFindSupplierbyUserIDandPhoneNumber(){
+        String phoneNumber = supplier.getPhoneNumber();
+        Long userID = newUser.getUserID();
+
+        Optional<Supplier> res = supplierReposTest.findSupplierByUserIDAndPhoneNumber(userID, phoneNumber);
+        assertEquals(res.get().getSupplierID(), supplier.getSupplierID());
     }
 }
--- Project/Buda/src/test/java/com/higroup/Buda/ServiceTest/SupplierServiceTest.java
+++ Project/Buda/src/test/java/com/higroup/Buda/ServiceTest/SupplierServiceTest.java
@@ -1,12 +1,17 @@
 package com.higroup.Buda.ServiceTest;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import com.higroup.Buda.entities.Supplier;
+import com.higroup.Buda.entities.User;
 import com.higroup.Buda.repositories.SupplierRepository;
+import com.higroup.Buda.repositories.UserRepository;
 import com.higroup.Buda.services.SupplierService;
 
 import org.junit.jupiter.api.AfterEach;
@@ -16,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -39,26 +45,80 @@ public class SupplierServiceTest {
 
     private SupplierService supplierService;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
     @AfterEach
     public void tearDown(){
         supplierRepository.deleteAll();
     }
 
 
-    public static Supplier supplier;
+    public Supplier supplier;
 
     @BeforeEach
-    void Setup(){
+    public void Setup(){
         supplierService = new SupplierService(supplierRepository);
     }
 
-    @BeforeAll
-    public static void initializeDB()
+    @BeforeEach
+    public void initializeDB()
     {
+        user = new User();
+        user.setEmail("haitq@gmail.com");
+        user.setFirstName("Hai");
+        user.setLastName("Tran");
+        user.setPassword("BBBBBasdsadBB");
+        user.setPhoneNumber("21312313");
+        user.setUserName("haihoho");
+        
         supplier = new Supplier();
-        supplier.setAddress("default");
-        supplier.setEmail("123456789@gmail.com");
-        supplier.setName("default");
-        supplier.setPhoneNumber("0123456789");
+        supplier.setName("nguyen Hoang Vu");
+        supplier.setEmail("nguyenhoangvudtm23@gmail.com");
+        supplier.setPhoneNumber("0367185122");
+
+        userRepository.save(user);
+        supplier.setUserID(user.getUserID());
+    }
+
+    @Test
+    public void canResigterNewSupplier(){
+        Long userID = user.getUserID();
+        long sizebeforeUpdate = supplierRepository.count();
+
+        ResponseEntity<?> res =  supplierService.registerNewSupplier(userID, supplier);
+
+        // check
+        assertEquals(res.getBody().toString(), supplier.toString());
+        assertEquals(sizebeforeUpdate + 1, supplierRepository.count());   
+        Supplier new_supplier = supplierRepository.findSupplierBySupplierID(supplier.getSupplierID()).get();
+        
+        assertEquals(new_supplier, supplier);
+    }
+
+    @Test 
+    public void canFindAllbyUserID(){
+        Long userID = user.getUserID();
+        supplierRepository.save(supplier);
+
+        // check
+        List<Supplier> list = supplierService.findAllByUserID(userID);
+
+        assertEquals(list, Arrays.asList(supplier));
+    }
+
+    @Test 
+    public void canFindSupplierbyUserIDandPhoneNumber(){
+        Long userID = user.getUserID();
+        String phoneNumber = supplier.getPhoneNumber();
+        supplierRepository.save(supplier);
+
+        // check
+        ResponseEntity<?> res = supplierService.findSupplierByUserIDAndPhoneNumber(userID, phoneNumber);
+        assertEquals(res.getBody().toString(), supplier.toString());
+
     }
 }
+
