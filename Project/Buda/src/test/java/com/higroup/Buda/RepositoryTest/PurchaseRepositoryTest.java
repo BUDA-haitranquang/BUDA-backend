a311acef2c2d3377786eab326f313fcbe2f140aa
linhngocnguyen <linh.nn194790@sis.hust.edu.vn>
2021-08-11 16:33:52 +0700

    Add some part of some tests

--- Project/Buda/src/test/java/com/higroup/Buda/RepositoryTest/PurchaseRepositoryTest.java
+++ Project/Buda/src/test/java/com/higroup/Buda/RepositoryTest/PurchaseRepositoryTest.java
@@ -2,10 +2,14 @@ package com.higroup.Buda.RepositoryTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 import com.higroup.Buda.entities.Purchase;
 import com.higroup.Buda.repositories.PurchaseRepository;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +34,29 @@ public class PurchaseRepositoryTest {
 
     @Autowired
     private PurchaseRepository purchaseReposTest;
+    @Autowired
+    private Purchase newPurchase;
+
+    @BeforeEach
+    public void Setup(){
+        Purchase newpurchase = new Purchase();
+        newpurchase.setMessage("New purchase");
+        newpurchase.setPurchaseID((long)2);
+        newpurchase.setTotalCost(350000.0);
+        newpurchase.setCreationDate(ZonedDateTime.parse("2021-07-20T10:15:30+07:00"));
+        purchaseReposTest.save(newpurchase);
+    }
 
     @AfterEach
     public void tearDown(){
         purchaseReposTest.deleteAll();
     }
-    @Test
-    void testFindPurchasebyPurchaseID(){
-        Purchase newpurchase = new Purchase();
-        
+
+    @Test 
+    public void canFindPurchasebyPurchaseID(){
+        Long PurchaseID = newPurchase.getPurchaseID();
+
+        Optional<Purchase> res = purchaseReposTest.findPurchaseByPurchaseID(PurchaseID);
+        assertEquals(res.get().getPurchaseID(), newPurchase.getPurchaseID());
     }
 }
---
+++ Project/Buda/src/test/java/com/higroup/Buda/RepositoryTest/SellOrderRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.higroup.Buda.RepositoryTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import com.higroup.Buda.entities.AgeGroup;
+import com.higroup.Buda.entities.Gender;
+import com.higroup.Buda.entities.SellOrder;
+import com.higroup.Buda.entities.Status;
+import com.higroup.Buda.entities.User;
+import com.higroup.Buda.repositories.SellOrderRepository;
+import com.higroup.Buda.repositories.UserRepository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.MySQLContainer;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class SellOrderRepositoryTest {
+    @Container
+    MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest")
+                    .withDatabaseName("new_db")
+                    .withUsername("testuser")
+                    .withPassword("pass");
+    @Autowired
+    private SellOrderRepository sellorderRepoTest;
+    @Autowired
+    private UserRepository userReposTest;
+
+    private User newUser;
+    private SellOrder newSellOrder;
+
+    @BeforeEach
+    public void SetUp(){
+        newUser = new User();
+        newUser.setEmail("linhnn@gmail.com");
+        newUser.setFirstName("Linh");
+        newUser.setLastName("Hai");
+        newUser.setPassword("BAehrfenwiof");
+        newUser.setPhoneNumber("9876543210");
+        newUser.setUserName("nnlinh");
+        userReposTest.save(newUser);
+  
+        newSellOrder = new SellOrder();
+        newSellOrder.setStatus(Status.FINISHED);
+        newSellOrder.setAgeGroup(AgeGroup.FROM_24_TO_30);
+        newSellOrder.setFinalCost(420000.0);
+        newSellOrder.setGender(Gender.MALE);
+        newSellOrder.setMessage("Excellent");
+        newSellOrder.setRealCost(420000.0);
+        newSellOrder.setUserID(newUser.getUserID());
+    }
+
+    @AfterEach
+    public void tearDown(){
+        sellorderRepoTest.deleteAll();
+        userReposTest.deleteAll();
+    }
+
+    @Test 
+    public void canFindSellOrderbySellOrderID(){
+        Long SellOrderID = newSellOrder.getSellOrderID();
+
+        Optional<SellOrder> res = sellorderRepoTest.findSellOrderBySellOrderID(SellOrderID);
+        assertEquals(res.get().getSellOrderID(), newSellOrder.getSellOrderID());
+    }
+
+    @Test 
+    public void canFindAllbyUserID(){
+        Long userID = newUser.getUserID();
+        List<SellOrder> list = sellorderRepoTest.findAllSellOrderByUserID(userID);
+
+        assertEquals(list, Arrays.asList(newSellOrder));
+    }
+}
