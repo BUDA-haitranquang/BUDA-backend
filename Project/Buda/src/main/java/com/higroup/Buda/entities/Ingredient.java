d71d088e04345acb7c76202b326265168a3724e3
linhngocnguyen <linh.nn194790@sis.hust.edu.vn>
2021-09-09 10:39:12 +0700

    Update IngredientLeftLog (entity, controller, repo, services)

---
+++ Project/Buda/src/main/java/com/higroup/Buda/entities/Ingredient.java
@@ -0,0 +1,132 @@
+package com.higroup.Buda.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "Ingredient")
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ingredientID;
+    @Column(length = 100)
+    private String name;
+    @Column(length = 1000)
+    private String description;
+    private int amountLeft;
+    private double price;
+    private Long storeID;
+    private Long pictureID;
+    private int alertAmountLeft;
+    @OneToMany(mappedBy = "ingredient", fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Set<BuyOrderItem> buyOrderItems;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(Long IngredientID, String name, String description, int amountLeft, double price, Long storeID, Long pictureID, int alertAmountLeft) {
+        this.ingredientID = IngredientID;
+        this.name = name;
+        this.description = description;
+        this.amountLeft = amountLeft;
+        this.price = price;
+        this.storeID = storeID;
+        this.pictureID = pictureID;
+        this.alertAmountLeft = alertAmountLeft;
+    }
+
+    public Long getIngredientID() {
+        return ingredientID;
+    }
+
+    public void setIngredientID(Long IngredientID) {
+        this.ingredientID = IngredientID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmountLeft() {
+        return amountLeft;
+    }
+
+    public void setAmountLeft(int amountLeft) {
+        this.amountLeft = amountLeft;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Long getStoreID() {
+        return storeID;
+    }
+
+    public void setStoreID(Long storeID) {
+        this.storeID = storeID;
+    }
+
+    public Long getPictureID() {
+        return pictureID;
+    }
+
+    public void setPictureID(Long pictureID) {
+        this.pictureID = pictureID;
+    }
+
+    public int getAlertAmountLeft() {
+        return alertAmountLeft;
+    }
+
+    public void setAlertAmountLeft(int alertAmountLeft) {
+        this.alertAmountLeft = alertAmountLeft;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return amountLeft == that.amountLeft && Double.compare(that.price, price) == 0 && alertAmountLeft == that.alertAmountLeft && Objects.equals(ingredientID, that.ingredientID) && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(storeID, that.storeID) && Objects.equals(pictureID, that.pictureID) && Objects.equals(buyOrderItems, that.buyOrderItems);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredientID, name, description, amountLeft, price, storeID, pictureID, alertAmountLeft, buyOrderItems);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "IngredientID=" + ingredientID +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", amountLeft=" + amountLeft +
+                ", price=" + price +
+                ", storeID=" + storeID +
+                ", pictureID=" + pictureID +
+                ", alertAmountLeft=" + alertAmountLeft +
+                '}';
+    }
+}
---
+++ Project/Buda/src/main/java/com/higroup/Buda/services/IngredientLeftLogService.java
@@ -0,0 +1,67 @@
+package com.higroup.Buda.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.higroup.Buda.entities.Ingredient;
+import com.higroup.Buda.entities.IngredientLeftLog;
+import com.higroup.Buda.entities.User;
+import com.higroup.Buda.repositories.IngredientLeftLogRepository;
+import com.higroup.Buda.repositories.IngredientRepository;
+import com.higroup.Buda.repositories.UserRepository;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IngredientLeftLogService {
+    private final IngredientLeftLogRepository ingredientLeftLogRepository;
+    private final UserRepository userRepository;
+    private final IngredientRepository ingredientRepository;
+    @Autowired
+    public IngredientLeftLogService(IngredientLeftLogRepository ingredientLeftLogRepository, UserRepository userRepository, IngredientRepository ingredientRepository)
+    {
+        this.userRepository = userRepository;
+        this.ingredientLeftLogRepository = ingredientLeftLogRepository;
+        this.ingredientRepository = ingredientRepository;
+    }
+    public ResponseEntity<?> findIngredientLeftLogByIngredientLeftLogID(Long ingredientLeftLogID)
+    {
+        Optional<IngredientLeftLog> ingredientLeftLog = this.ingredientLeftLogRepository.findIngredientLeftLogByIngredientLeftLogID(ingredientLeftLogID);
+        if (ingredientLeftLog.isPresent())
+        {
+            return ResponseEntity.ok().body(ingredientLeftLog.get().toString());
+        }
+        return ResponseEntity.badRequest().body("Not found");
+    }
+    public ResponseEntity<?> registerNewIngredientLeftLog(Long userID, IngredientLeftLog ingredientLeftLog)
+    {
+        Optional<User> user = this.userRepository.findUserByUserID(userID);
+        if (!user.isPresent())
+        {
+            return ResponseEntity.badRequest().body("User not found");
+        }
+        ingredientLeftLog.setUserID(userID);
+        this.ingredientLeftLogRepository.save(ingredientLeftLog);
+        return ResponseEntity.ok().body(ingredientLeftLog.toString());
+    }
+    public List<IngredientLeftLog> findAllIngredientLeftLogByIngredient(Long ingredientID)
+    {
+        Optional<Ingredient> Ingredient = this.ingredientRepository.findIngredientByIngredientID(ingredientID);
+        if (!Ingredient.isPresent())
+        {
+            return null;
+        }
+        return this.ingredientLeftLogRepository.findAllIngredientLeftLogByIngredient(Ingredient.get());
+    }
+    public List<IngredientLeftLog> findAllIngredientLeftLogByStaffID(Long staffID)
+    {
+        return this.ingredientLeftLogRepository.findAllIngredientLeftLogByStaffID(staffID);
+    }
+    public List<IngredientLeftLog> findAllIngredientLeftLogByUserID(Long userID)
+    {
+        return this.ingredientLeftLogRepository.findAllIngredientLeftLogByUserID(userID);
+    }
+}
