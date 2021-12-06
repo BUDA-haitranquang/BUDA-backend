c955fa78298e5a51dac648e066cf94da997464fe
linhngocnguyen <linh.nn194790@sis.hust.edu.vn>
2021-12-06 22:12:58 +0700

    Update event update membership

--- DatabaseDemo/events.sql
+++ DatabaseDemo/events.sql
@@ -17,3 +17,14 @@ inner join
 (select discount_id, count(distinct sell_order_id) as res from sell_order group by discount_id) as s
 on d.discount_id = s.discount_id
 set d.order_count = s.res;
+
+CREATE EVENT daily_membership_update
+on schedule
+EVERY 1 HOUR
+DO 
+Update customer AS c 
+Set membership_id=
+(select membership_type_id from membership_type as m
+where m.user_id=c.user_id
+and c.total_spend-m.minimum_spend>0
+order by c.total_spend-m.minimum_spend limit 1);
