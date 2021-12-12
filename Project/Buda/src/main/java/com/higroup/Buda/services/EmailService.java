e4288ccc2d03ebe1b30f73924b7300b7d805a4b2
hieutran29 <hieu.xteen28@gmail.com>
2021-12-12 15:08:46 +0700

    Added EmailService to handle sending emails

---
+++ Project/Buda/src/main/java/com/higroup/Buda/services/EmailService.java
@@ -0,0 +1,33 @@
+package com.higroup.Buda.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Async
+    public void send(String to, String subject, String content) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+
+            helper.setText(content);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setFrom("rokclone201@gmail.com");
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new IllegalStateException("failed to send email");
+        }
+    }
+}
