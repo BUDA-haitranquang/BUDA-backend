29e5f65b7794b6c732942ede9d8dedfe37908041
tranhaidang <44207868+xfa4@users.noreply.github.com>
2022-01-13 23:27:47 +0700

    Add UpdateUserPassword
    When user is logged in, user can change the current password.

--- Project/Buda/src/main/java/com/higroup/Buda/api/user/password/update/UpdateUserPasswordService.java
+++ Project/Buda/src/main/java/com/higroup/Buda/api/user/password/update/UpdateUserPasswordService.java
@@ -1,5 +1,47 @@
 package com.higroup.Buda.api.user.password.update;
 
+import com.higroup.Buda.customDTO.UserUpdatePassword;
+import com.higroup.Buda.entities.User;
+import com.higroup.Buda.repositories.UserRepository;
+import com.higroup.Buda.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
 public class UpdateUserPasswordService {
-    
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
+
+
+    @Autowired
+    public UpdateUserPasswordService(UserRepository userRepository, JwtTokenUtil jwtTokenUtil) {
+        this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        this.userRepository = userRepository;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+    public void updateUserPassword(Long userID, UserUpdatePassword userUpdatePassword)
+    {
+        Optional<User> user = userRepository.findUserByUserID(userID);
+        if (user.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        String rawPassword = userUpdatePassword.getCurrentPassword();
+        if (bCryptPasswordEncoder.matches(rawPassword, user.get().getPassword()) && user.get().isEnabled())
+        {
+//            System.out.println(userUpdatePassword.getNewPassword() + ' ' + userUpdatePassword.getConfirmNewPassword());
+            if (userUpdatePassword.getNewPassword().equals(userUpdatePassword.getConfirmNewPassword()))
+            {
+                user.get().setPassword(bCryptPasswordEncoder.encode(userUpdatePassword.getNewPassword()));
+                this.userRepository.save(user.get());
+                return;
+            }
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password not matching");
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong password");
+    }
 }
