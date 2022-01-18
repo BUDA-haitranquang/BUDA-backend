8262087e8f3d24c1502f1a71c027f380c71aa201
Hai Tran Quang <75372438+haitranquang-official@users.noreply.github.com>
2022-01-18 19:31:57 +0700

    findAll test samples

--- Project/Buda/src/test/java/com/higroup/Buda/api/business/payslip/view/ViewPaySlipServiceTest.java
+++ Project/Buda/src/test/java/com/higroup/Buda/api/business/payslip/view/ViewPaySlipServiceTest.java
@@ -1,6 +1,11 @@
 package com.higroup.Buda.api.business.payslip.view;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import com.higroup.Buda.BudaApplication;
+import com.higroup.Buda.entities.PaySlip;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +15,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BudaApplication.class)
 public class ViewPaySlipServiceTest {
+    private ViewPaySlipService viewPaySlipService;
     @Test
     void testFindAllPaySlipByUserID() {
-
+        List<PaySlip> paySlips = this.viewPaySlipService.findAllPaySlipByUserID(2l);
+        for (PaySlip paySlip: paySlips){
+            assertEquals(paySlip.getUserID(), 2l);
+        }
     }
 
     @Test
--- Project/Buda/src/test/java/com/higroup/Buda/api/business/receipt/view/ViewReceiptServiceTest.java
+++ Project/Buda/src/test/java/com/higroup/Buda/api/business/receipt/view/ViewReceiptServiceTest.java
@@ -1,17 +1,28 @@
 package com.higroup.Buda.api.business.receipt.view;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import com.higroup.Buda.BudaApplication;
+import com.higroup.Buda.entities.Receipt;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BudaApplication.class)
 public class ViewReceiptServiceTest {
+    @Autowired
+    private ViewReceiptService viewReceiptService;
     @Test
     void testFindAllReceiptByUserID() {
-
+        List<Receipt> receipts = this.viewReceiptService.findAllReceiptByUserID(2l);
+        for (Receipt receipt: receipts){
+            assertEquals(receipt.getUserID(), 2l);
+        }
     }
 }
