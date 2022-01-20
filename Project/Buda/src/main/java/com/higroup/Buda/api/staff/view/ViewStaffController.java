1ce5bb9756e4d531707769a1f24f13d4f5b45446
haitranquang-official <haitranquang.official@gmail.com>
2022-01-20 05:27:32 +0000

    Find staff by staffID

--- Project/Buda/src/main/java/com/higroup/Buda/api/staff/view/ViewStaffController.java
+++ Project/Buda/src/main/java/com/higroup/Buda/api/staff/view/ViewStaffController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,10 @@ public class ViewStaffController {
     public ResponseEntity<?> findAllStaffByCurrentUser(HttpServletRequest httpServletRequest){
         Long userID = this.requestUtil.getUserIDFromUserToken(httpServletRequest);
         return ResponseEntity.ok().body(this.viewStaffService.findAllStaffByUserID(userID));
+    }
+    @GetMapping("/id/{staffID}")
+    public ResponseEntity<?> findStaffByStaffID(HttpServletRequest httpServletRequest, @PathVariable Long staffID){
+        Long userID = this.requestUtil.getUserIDFromUserToken(httpServletRequest);
+        return ResponseEntity.ok().body(this.viewStaffService.findStaffByStaffID(userID, staffID));
     }
 }
--- Project/Buda/src/main/java/com/higroup/Buda/api/staff/view/ViewStaffService.java
+++ Project/Buda/src/main/java/com/higroup/Buda/api/staff/view/ViewStaffService.java
@@ -1,12 +1,15 @@
 package com.higroup.Buda.api.staff.view;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.higroup.Buda.entities.Staff;
 import com.higroup.Buda.repositories.StaffRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ViewStaffService {
@@ -17,5 +20,12 @@ public class ViewStaffService {
     }
     public List<Staff> findAllStaffByUserID(Long userID){
         return this.staffRepository.findAllByUserID(userID);
+    }
+    public Staff findStaffByStaffID(Long userID, Long staffID){
+        Optional<Staff> staffOptional = this.staffRepository.findStaffByStaffID(staffID);
+        if ((staffOptional.isPresent()) && (staffOptional.get().getUserID().equals(userID))){
+            return staffOptional.get();
+        }
+        else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not found");
     }
 }
