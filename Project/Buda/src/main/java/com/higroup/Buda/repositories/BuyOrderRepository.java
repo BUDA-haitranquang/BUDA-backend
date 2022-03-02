0a82c05d044b89a4288bbba6c2a01545d52b1cb0
nguyenhoangvudtm23 <68849645+nguyenhoangvudtm23@users.noreply.github.com>
2022-03-02 15:00:01 +0700

    check date query buyorderRepository

--- Project/Buda/src/main/java/com/higroup/Buda/repositories/BuyOrderRepository.java
+++ Project/Buda/src/main/java/com/higroup/Buda/repositories/BuyOrderRepository.java
@@ -19,7 +19,7 @@ public interface BuyOrderRepository extends JpaRepository<BuyOrder, Long> {
     List<BuyOrder> findAllBuyOrderByUserIDAndStatus(Long userID, Status status);
     @Query(value = "select * from buy_order s where s.status LIKE :status and s.user_id = :userID", nativeQuery = true)
     List<BuyOrder> findAllBuyOrderByStatusAndUserID(@Param("userID") Long userID, @Param("status") String status);
-    @Query(value = "select * from buy_order s where (s.creation_time BETWEEN NOW() - INTERVAL :X DAY and NOW()) and s.user_id = :userID", nativeQuery = true)
+    @Query(value = "select * from buy_order s where (s.creation_time BETWEEN CAST((NOW() - INTERVAL :X DAY) as DATE) and NOW()) and s.user_id = :userID", nativeQuery = true)
     List<BuyOrder> findAllBuyOrderByUserIDLastXDays(@Param("userID") Long userID, @Param("X") Long X);
     @Query(value = "select * from buy_order s where s.status NOT LIKE 'FINISHED' and s.status NOT LIKE 'CANCELLED' and s.user_id = :userID", nativeQuery = true)
     List<BuyOrder> findAllIncompletedBuyOrderByUser(@Param("userID") Long userID);
@@ -31,7 +31,7 @@ public interface BuyOrderRepository extends JpaRepository<BuyOrder, Long> {
     + "order by DATE_FORMAT(f.creationTime, '%V-%Y') ")
     List<ExpenseByTimeStatistics> findBuyOrderExpenseByWeek(@Param("userID") Long userID);
     @Query(value = "select new com.higroup.Buda.customDTO.ExpenseByTimeStatistics(DATE_FORMAT(f.creationTime, '%d-%m-%Y'), SUM(f.totalCost)) "
-    + "from BuyOrder f where f.userID = :userID and month(f.creationTime) = month(current_date) "
+    + "from BuyOrder f where f.userID = :userID and year(f.creationTime) = year(current_date) and month(f.creationTime) = month(current_date) "
     + "group by DATE_FORMAT(f.creationTime, '%d-%m-%Y') "
     + "order by DATE_FORMAT(f.creationTime, '%d-%m-%Y') ")
     List<ExpenseByTimeStatistics> findBuyOrderExpenseCurrentMonth(@Param("userID") Long userID);
