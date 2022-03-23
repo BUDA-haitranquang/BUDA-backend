b96121e5976598de82c7122aea5f9d7c174601d9
Hai Tran Quang <75372438+haitranquang-official@users.noreply.github.com>
2022-03-23 09:01:54 +0700

    Tính SellOrderRevenue trong khoảng thời gian từ A->B

--- Project/Buda/src/main/java/com/higroup/Buda/api/statistics/revenue/sellorder/SellOrderRevenueService.java
+++ Project/Buda/src/main/java/com/higroup/Buda/api/statistics/revenue/sellorder/SellOrderRevenueService.java
@@ -1,7 +1,9 @@
 package com.higroup.Buda.api.statistics.revenue.sellorder;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
+import com.higroup.Buda.customDTO.PeriodDTO;
 import com.higroup.Buda.customDTO.RevenueByTimeStatistics;
 import com.higroup.Buda.repositories.SellOrderRepository;
 
@@ -31,5 +33,10 @@ public class SellOrderRevenueService {
     public List<RevenueByTimeStatistics> findRevenueAllDaysCurrentMonth(Long userID)
     {
         return this.sellOrderRepository.findRevenueAllDaysCurrentMonth(userID);
+    }
+    public List<RevenueByTimeStatistics> findRevenueByPeriod(Long userID, PeriodDTO periodDTO){
+        ZonedDateTime from = periodDTO.getFrom().withHour(0).withMinute(0).withSecond(0);
+        ZonedDateTime to = periodDTO.getTo().withHour(0).withMinute(0).withSecond(0);
+        return this.sellOrderRepository.findRevenueInPeriod(userID, from, to);
     }
 }
