0e8a763f43061213abed365a882f41197d1bf6c1
haitranquang-official <haitranquang.official@gmail.com>
2022-05-22 02:35:58 +0000

    Update & View Store

--- Project/Buda/src/main/java/com/higroup/Buda/api/store/view/ViewStoreService.java
+++ Project/Buda/src/main/java/com/higroup/Buda/api/store/view/ViewStoreService.java
@@ -6,7 +6,9 @@ import com.higroup.Buda.entities.Store;
 import com.higroup.Buda.repositories.StoreRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ViewStoreService {
@@ -17,5 +19,12 @@ public class ViewStoreService {
     }
     public List<Store> findAllStoresByUser(Long userID) {
         return this.storeRepository.findAllByUserID(userID);
+    }
+    public Store findOneStoreByUser(Long userID) {
+        Store store = this.storeRepository.findAllByUserID(userID).get(0);
+        if (store != null) {
+            return store;
+        }
+        else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This user has no stores");
     }
 }
