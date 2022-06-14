394d73842dd3c638cc43757a844dbf3eb1cf1e63
Hai Tran Quang <75372438+haitranquang-official@users.noreply.github.com>
2022-06-14 08:16:19 +0700

    Replace where != null with Having not null

--- Project/Buda/src/main/java/com/higroup/Buda/api/statistics/product/best/selling/BestSellingProductStatisticsRepository.java
+++ Project/Buda/src/main/java/com/higroup/Buda/api/statistics/product/best/selling/BestSellingProductStatisticsRepository.java
@@ -26,8 +26,9 @@ public interface BestSellingProductStatisticsRepository extends JpaRepository<Se
     + "sum(if(s.status = 'RETURNED', i.quantity, 0)) as returnNumber, "
     + "sum(if(s.status = 'RETURNED', i.price_per_unit * i.quantity, 0)) as returnPrice "
     + "from sell_order_item i " + "inner join product p on i.product_id = p.product_id " + "inner join sell_order s on s.sell_order_id = i.sell_order_id "
-    + "where profit != null and i.user_id = :userID and s.finish_time BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) and now() "
+    + "where i.user_id = :userID and s.finish_time BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) and now() "
     + "group by i.product_id "
+    + "having profit is not null and revenue is not null "
     + "order by sellNumber desc, p.name asc "
     + "limit :number", nativeQuery = true)
     List<ProductRanking> findNProductsTopSellNumber(@Param("userID") Long userID, @Param("number") Integer number);
