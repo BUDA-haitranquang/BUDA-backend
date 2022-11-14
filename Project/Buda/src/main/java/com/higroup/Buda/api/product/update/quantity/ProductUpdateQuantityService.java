d5b5b5cd4583c423f59ab39ad830a4016ac04196
Tien4857 <tiennd@sapo.vn>
2022-11-14 14:50:09 +0700

    refactor: detach function to de-duplication

--- Project/Buda/src/main/java/com/higroup/Buda/api/product/update/quantity/ProductUpdateQuantityService.java
+++ Project/Buda/src/main/java/com/higroup/Buda/api/product/update/quantity/ProductUpdateQuantityService.java
@@ -1,32 +1,27 @@
 package com.higroup.Buda.api.product.update.quantity;
 
-import java.time.ZonedDateTime;
-import java.util.Objects;
-import java.util.Optional;
-
-import javax.transaction.Transactional;
-
 import com.higroup.Buda.entities.Product;
 import com.higroup.Buda.entities.ProductLeftLog;
-import com.higroup.Buda.entities.User;
 import com.higroup.Buda.repositories.ProductLeftLogRepository;
 import com.higroup.Buda.repositories.ProductRepository;
-import com.higroup.Buda.repositories.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+import java.util.Optional;
+
 @Service
 public class ProductUpdateQuantityService {
     private final ProductRepository productRepository;
-    private final UserRepository userRepository;
     private final ProductLeftLogRepository productLeftLogRepository;
+
     @Autowired
-    public ProductUpdateQuantityService(ProductRepository productRepository, UserRepository userRepository, ProductLeftLogRepository productLeftLogRepository)
+    public ProductUpdateQuantityService(ProductRepository productRepository, ProductLeftLogRepository productLeftLogRepository)
     {
-        this.userRepository = userRepository;
         this.productRepository = productRepository;
         this.productLeftLogRepository = productLeftLogRepository;
     }
@@ -34,19 +29,13 @@ public class ProductUpdateQuantityService {
     public Product editProductQuantity(Long userID, Long productID, Integer amountLeftChange, String message)
     {
         Optional<Product> opProduct = this.productRepository.findProductByProductID(productID);
-        if(!opProduct.isPresent()){
+        if(opProduct.isEmpty()){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
         }
         Product product = opProduct.get();
         if (Objects.equals(product.getUserID(), userID))
         {
-            Integer amountLeft = product.getAmountLeft();
-            if (amountLeft + amountLeftChange < 0)
-            {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not enough amount for the request");
-            }
-            product.setAmountLeft(amountLeft + amountLeftChange);
-            this.productRepository.save(product);
+            updateProductAmountLeft(product, amountLeftChange);
             ProductLeftLog productLeftLog = new ProductLeftLog();
             productLeftLog.setProduct(product);
             productLeftLog.setAmountLeftChange(amountLeftChange);
@@ -57,5 +46,16 @@ public class ProductUpdateQuantityService {
             return product;
         }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not belong to user");
+    }
+
+    @Transactional
+    public void updateProductAmountLeft(Product product, Integer amountLeftChange) {
+        Integer amountLeft = product.getAmountLeft();
+        if (amountLeft + amountLeftChange < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not enough amount for the request");
+        }
+        product.setAmountLeft(amountLeft + amountLeftChange);
+
+        productRepository.save(product);
     }
 }
